1bac573c6ad2a673b8e6cbe4564af30d5db2905c
kei1_st <kei1satoh0603@gmail.com>
2024-03-20 00:36:21 +0900

    1つのボタンのみが clicked になるよう変更

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,19 @@
 "use client";
 
-import { useState } from "react";
 import { ReactNode } from "react";
 
-export default function Button({ children }: { children: ReactNode }) {
-  const [isButtonClicked, setButtonClicked] = useState(false);
-
-  function clickHandler() {
-    setButtonClicked(!isButtonClicked);
-    console.log("isButtonClicked: ", isButtonClicked);
-  }
-
+export default function Button({
+  children,
+  clickHandler,
+  isClicked,
+}: {
+  children: ReactNode;
+  id: number;
+  clickHandler: () => void;
+  isClicked: boolean;
+}) {
   return (
-    <button
-      className={`w-20 h-10 rounded-t-lg text-sm ${isButtonClicked ? "shadow-p" : "shadow-np"}`}
-      onClick={clickHandler}
-    >
+    <button className={`w-20 h-10 rounded-t-lg text-sm ${isClicked ? "shadow-p" : "shadow-np"}`} onClick={clickHandler}>
       {children}
     </button>
   );
--- components/Contents.tsx
+++ components/Contents.tsx
@@ -1,12 +1,55 @@
+"use client";
+
+import { useState } from "react";
+
 import Button from "./Button";
 
 export default function Section() {
+  const [clickedButton, setButtonClicked] = useState(0);
+
+  function clickHandler(id: number) {
+    setButtonClicked(id);
+    console.log("clickedButton: ", clickedButton);
+  }
+
   return (
     <div className="flex">
-      <Button>にってい</Button>
-      <Button>にゅーす</Button>
-      <Button>しゃしん</Button>
-      <Button>りんく集</Button>
+      <Button
+        id={0}
+        clickHandler={() => {
+          clickHandler(0);
+        }}
+        isClicked={clickedButton == 0}
+      >
+        にってい
+      </Button>
+      <Button
+        id={1}
+        clickHandler={() => {
+          clickHandler(1);
+        }}
+        isClicked={clickedButton == 1}
+      >
+        にゅーす
+      </Button>
+      <Button
+        id={2}
+        clickHandler={() => {
+          clickHandler(2);
+        }}
+        isClicked={clickedButton == 2}
+      >
+        しゃしん
+      </Button>
+      <Button
+        id={3}
+        clickHandler={() => {
+          clickHandler(3);
+        }}
+        isClicked={clickedButton == 3}
+      >
+        りんく集
+      </Button>
     </div>
   );
 }
