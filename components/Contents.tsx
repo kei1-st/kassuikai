37da08edabb8875372f95682cc9fdc053823403f
kei1_st <kei1satoh0603@gmail.com>
2024-03-20 01:32:34 +0900

    ボタン表示をmapで処理するよう変更

--- components/Contents.tsx
+++ components/Contents.tsx
@@ -12,44 +12,22 @@ export default function Section() {
     console.log("clickedButton: ", clickedButton);
   }
 
+  const tabNames = ["にってい", "にゅーす", "しゃしん", "りんく集"];
+
   return (
     <div className="flex">
-      <Button
-        id={0}
-        clickHandler={() => {
-          clickHandler(0);
-        }}
-        isClicked={clickedButton == 0}
-      >
-        にってい
-      </Button>
-      <Button
-        id={1}
-        clickHandler={() => {
-          clickHandler(1);
-        }}
-        isClicked={clickedButton == 1}
-      >
-        にゅーす
-      </Button>
-      <Button
-        id={2}
-        clickHandler={() => {
-          clickHandler(2);
-        }}
-        isClicked={clickedButton == 2}
-      >
-        しゃしん
-      </Button>
-      <Button
-        id={3}
-        clickHandler={() => {
-          clickHandler(3);
-        }}
-        isClicked={clickedButton == 3}
-      >
-        りんく集
-      </Button>
+      {tabNames.map((name, idx) => (
+        <Button
+          key={idx}
+          id={idx}
+          clickHandler={() => {
+            clickHandler(idx);
+          }}
+          isClicked={clickedButton == idx}
+        >
+          {name}
+        </Button>
+      ))}
     </div>
   );
 }
