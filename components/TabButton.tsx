2782e927de0e2967011774576565974ea55ce404
kei1_st <kei1satoh0603@gmail.com>
2024-03-20 01:57:00 +0900

    フォーマット

--- components/TabButton.tsx
+++ components/TabButton.tsx
@@ -13,7 +13,10 @@ export default function Button({
   isClicked: boolean;
 }) {
   return (
-    <button className={`w-20 h-10 rounded-t-lg text-sm ${isClicked ? "shadow-p bg-pushed" : "shadow-np"}`} onClick={clickHandler}>
+    <button
+      className={`w-20 h-10 rounded-t-lg text-sm ${isClicked ? "shadow-p bg-pushed" : "shadow-np"}`}
+      onClick={clickHandler}
+    >
       {children}
     </button>
   );
--- components/TabContent.tsx
+++ components/TabContent.tsx
@@ -1,7 +1,7 @@
-export default function TabContent (){
+export default function TabContent() {
   return (
     <div className="justify-center shadow-contents h-64 rounded-b-lg">
       <p>test</p>
     </div>
-  )
+  );
 }
