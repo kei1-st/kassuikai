64b7699cc9ac3bc30c94b5b7404fa30d769b295b
kei1-st <kei1satoh0603@gmail.com>
2024-03-24 16:51:09 +0900

    コンテンツをコンポーネントの中央に配置

--- components/TabContent.tsx
+++ components/TabContent.tsx
@@ -1,7 +1,7 @@
 export default function TabContent() {
   return (
-    <div className="justify-center shadow-contents h-96 rounded-b-lg">
-      <p>test</p>
+    <div className="justify-center shadow-contents h-96 rounded-b-lg place-content-center">
+      <p className="text-center">Maybe coming soon.</p>
     </div>
   );
 }
