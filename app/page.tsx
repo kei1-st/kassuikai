fdc00bd1e252e0519e4f3045ed2c52b298f9c883
kei1-st <kei1satoh0603@gmail.com>
2024-03-30 18:13:14 +0900

    カレンダーからのイベント取得を仮実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
 import Image from "next/image";
 
+import { getEventListFromGoogleCalendar } from "@/scripts/calendar_auth";
+
 import Contents from "../components/Contents";
 
+async function fetchEvents() {
+  const timeMin = new Date().toISOString();
+  // 例として、1週間後の日付をtimeMaxとして設定
+  const timeMax = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000).toISOString();
+
+  try {
+    const events = await getEventListFromGoogleCalendar(timeMin, timeMax);
+    console.log(events);
+  } catch (error) {
+    console.error("error occured while fetching events. Error details: ", error);
+  }
+}
+
 export default function Home() {
+  fetchEvents();
   return (
     <main className="flex min-h-screen w-screen flex-col items-center p-10 space-y-10">
       <Image className="mt-10" src="/isuinu_logo.svg" alt="Isuinu Logo" width={200} height={200} />
---
+++ scripts/calendar_auth.ts
@@ -0,0 +1,63 @@
+import dotenv from "dotenv";
+import { google } from "googleapis";
+
+dotenv.config();
+
+// 環境変数から認証情報を取得
+const clientId = process.env.GOOGLE_OAUTH_CLIENT_ID;
+const clientSecret = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
+const refreshToken = process.env.GOOGLE_OAUTH_REFRESH_TOKEN;
+const calendarId = process.env.CALENDAR_ID;
+
+// 認証のための関数
+const getGoogleOAuth = async () => {
+  const googleOAuth = new google.auth.OAuth2(clientId, clientSecret, "http://localhost");
+
+  // 毎回のリクエスト時に新しいアクセストークンを取得
+  googleOAuth.setCredentials({
+    refresh_token: refreshToken,
+  });
+
+  try {
+    const accessTokenResponse = await googleOAuth.getAccessToken();
+
+    const accessToken = accessTokenResponse.token;
+
+    if (!accessToken) throw new Error("couldn't fetch valid access tokens");
+
+    googleOAuth.setCredentials({
+      access_token: accessToken,
+    });
+
+    return googleOAuth;
+  } catch (err) {
+    console.log("error details: ", err);
+    throw new Error("error occured while fetching access tokens.");
+  }
+};
+
+/**
+ * @param timeMin new Date().toISOString() etc...
+ * @param timeMax new Date().toISOString() etc...
+ */
+export const getEventListFromGoogleCalendar = async (timeMin: string, timeMax: string) => {
+  try {
+    // 認証
+    const googleOAuth = await getGoogleOAuth();
+
+    const calendar = google.calendar({ version: "v3", auth: googleOAuth });
+
+    const res = await calendar.events.list({
+      calendarId,
+      timeMin,
+      timeMax,
+      timeZone: "Asia/Tokyo",
+    });
+
+    if (!res.data.items) throw new Error("couldn't fetch events.");
+
+    return res.data.items;
+  } catch (err) {
+    console.log("error occured while fetching events from the calendar", err);
+  }
+};
