45d0cde53da626a0dd2202fa8cc81354aef34ede
kei1-st <kei1satoh0603@gmail.com>
2024-03-30 19:23:44 +0900

    ビルドエラー対処

--- app/page.tsx
+++ app/page.tsx
@@ -18,8 +18,8 @@ async function fetchEvents() {
 }
 
 export default function Home() {
-  fetchEvents();
-  return (
+  void fetchEvents(); // エラーハンドリングは定義側でやっているので未処理の Promises を void で明示的に無視する
+  return ( 
     <main className="flex min-h-screen w-screen flex-col items-center p-10 space-y-10">
       <Image className="mt-10" src="/isuinu_logo.svg" alt="Isuinu Logo" width={200} height={200} />
       <Contents />
