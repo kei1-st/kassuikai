8cc5671587b84606da7c44fef15a4418ac76cc64
kei1-st <kei1satoh0603@gmail.com>
2024-03-31 14:31:03 +0900

    レスポンシブ設定の修正

--- components/Event.tsx
+++ components/Event.tsx
@@ -5,7 +5,7 @@ import { notojp } from "@/utils/fonts";
 
 export default function Event({ event }: { event: calendar_v3.Schema$Event }) {
   return (
-    <div className={`mx-5 my-2 ${notojp.className} text-[min(3vw,150px)`}>
+    <div className={`mx-5 my-2 ${notojp.className} text-[min(3vw,20px)]`}>
       {format_event_date(event.start?.dateTime, event.end?.dateTime)} &nbsp; {event.summary}
     </div>
   );
