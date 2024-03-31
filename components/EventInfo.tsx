048b33445bc109fe8b2f1e495cfc0adc3a37905e
kei1-st <kei1satoh0603@gmail.com>
2024-03-31 17:39:21 +0900

    リネーム

--- components/EventInfo.tsx
+++ components/EventInfo.tsx
@@ -1,11 +1,10 @@
 import { calendar_v3 } from "googleapis";
 
 import { format_event_date } from "@/scripts/format_date";
-import { notojp } from "@/utils/fonts";
 
-export default function Event({ event }: { event: calendar_v3.Schema$Event }) {
+export default function EventInfo({ event }: { event: calendar_v3.Schema$Event }) {
   return (
-    <div className={`mx-5 my-2 ${notojp.className} text-[min(3vw,15px)]`}>
+    <div className={`mx-5 my-2`}>
       {format_event_date(event.start?.dateTime, event.end?.dateTime)} &nbsp; {event.summary}
     </div>
   );
