4081abffeb202b5ab583670c887b35d0899dfe1f
kei1-st <kei1satoh0603@gmail.com>
2024-03-31 20:17:27 +0900

    Revert "Merge pull request #14 from kei1-st/kei1-st/issue013"
    This reverts commit 39662c7e107d41b12e4c97398c6c8891a1e51666, reversing
    changes made to 8ad0fd14dbbbdff7533ded72c68e8fd95458f45d.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,4 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-const nextConfig = {
-  // Your Next.js configuration here
-};
-export default withBundleAnalyzer(nextConfig);
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
