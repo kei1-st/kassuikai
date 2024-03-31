bead29f293c5e91df1a2d37bdc7cda571d898faa
kei1-st <kei1satoh0603@gmail.com>
2024-03-31 18:23:36 +0900

    縦幅の調整

--- components/ComingSoonTab.tsx
+++ components/ComingSoonTab.tsx
@@ -1,7 +1,7 @@
 export default function ComingSoonTab() {
   return (
-    <p className="justify-center shadow-contents h-96 rounded-b-lg place-content-center text-center">
-      Maybe coming soon.
+    <p className="flex flex-wrap justify-center shadow-contents h-[max(3vh,262px)] pb-10 rounded-b-lg place-content-center text-[min(3vw,15px)] px-3 py-10 space-y-5 md:space-y-0">
+      no information
     </p>
   );
 }
