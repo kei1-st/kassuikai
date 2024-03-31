f6bf5ac44ffd0bf138a1c9de6adcbdba5bfa7b15
kei1-st <kei1satoh0603@gmail.com>
2024-03-31 13:49:27 +0900

    noto sans jp を使用するよう変更

--- components/Event.tsx
+++ components/Event.tsx
@@ -2,9 +2,11 @@ import { calendar_v3 } from "googleapis";
 
 import { format_event_date } from "@/scripts/format_date";
 
+import { notojp } from "@/utils/fonts";
+
 export default function Event({ key, event }: { key: string; event: calendar_v3.Schema$Event }) {
   return (
-    <div className="mx-5 my-2" key={key} style={{ fontFamily: "Roboto", fontSize: "14px" }}>
+    <div className={`mx-5 my-2 ${notojp.className}`} key={key}>
       {format_event_date(event.start?.dateTime, event.end?.dateTime)} &nbsp; {event.summary}
     </div>
   );
---
+++ utils/fonts.ts
@@ -0,0 +1,8 @@
+import { Noto_Sans_JP } from "next/font/google";
+
+export const notojp = Noto_Sans_JP({
+    weight: ["300"],
+    subsets: ["latin"],
+    display: "swap",
+  });
+  
