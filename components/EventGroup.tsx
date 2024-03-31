0f3b2d59e76b48fc8ce1223fd8b7a511954a0e90
kei1-st <kei1satoh0603@gmail.com>
2024-03-31 18:09:51 +0900

    表示を調整

--- components/EventGroup.tsx
+++ components/EventGroup.tsx
@@ -5,7 +5,7 @@ import EventInfo from "./EventInfo";
 export default function EventGroup({ title, events }: { title: string; events: calendar_v3.Schema$Event[] }) {
   return (
     <>
-      <h1> {title} </h1>
+      <h1 className="mx-5"> 直近の{title} </h1>
       {events.map((event) => {
         return event.id && event.organizer?.displayName === title && <EventInfo key={event.id} event={event} />;
       })}
--- components/EventTab.tsx
+++ components/EventTab.tsx
@@ -8,10 +8,14 @@ export default function EventTab({ events }: { events: calendar_v3.Schema$Event[
   if (events) {
     return (
       <div
-        className={`justify-center shadow-contents h-96 rounded-b-lg place-content-center ${notojp.className} text-[min(3vw,15px)]`}
+        className={`flex flex-wrap justify-center shadow-contents h-3vh pb-10 rounded-b-lg place-content-center ${notojp.className} text-[min(3vw,15px)] px-3 py-10 space-y-5 md:space-y-0`}
       >
-        <EventGroup title="練習日程" events={events} />
-        <EventGroup title="新歓日程" events={events} />
+        <div className="w-full md:w-1/2 md:px-8">
+          <EventGroup title="練習日程" events={events} />
+        </div>
+        <div className="w-full md:w-1/2">
+          <EventGroup title="新歓日程" events={events} />
+        </div>
       </div>
     );
   } else {
