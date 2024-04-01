3a726abc07af21cab826de98e8993038034528d5
kei1-st <kei1satoh0603@gmail.com>
2024-04-01 13:13:18 +0900

    曜日を修正

--- scripts/format_date.ts
+++ scripts/format_date.ts
@@ -1,19 +1,19 @@
 function conv2DOW(num: number) {
   switch (num) {
     case 0:
-      return "月";
-    case 1:
-      return "火";
-    case 2:
-      return "水";
-    case 3:
-      return "木";
-    case 4:
-      return "金";
-    case 5:
-      return "土";
-    case 6:
       return "日";
+    case 1:
+      return "月";
+    case 2:
+      return "火";
+    case 3:
+      return "水";
+    case 4:
+      return "木 ";
+    case 5:
+      return "金";
+    case 6:
+      return "土";
     default:
       return "";
   }
