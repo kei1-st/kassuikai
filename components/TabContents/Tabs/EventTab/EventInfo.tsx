c6b73477b86cf5aafe9c5d900601fda1eef31027
kei1-st <kei1satoh0603@gmail.com>
2024-04-04 18:59:19 +0900

    MapLink をコンポーネント化

--- components/TabContents/Tabs/EventTab/EventInfo.tsx
+++ components/TabContents/Tabs/EventTab/EventInfo.tsx
@@ -1,8 +1,8 @@
 import { calendar_v3 } from "googleapis";
-import Image from "next/image";
 
 import { format_event_date } from "@/scripts/format_date";
-import formatLocation from "@/scripts/format_location";
+
+import MapLink from "./MapLink";
 
 export default function EventInfo({
   event,
@@ -18,15 +18,7 @@ export default function EventInfo({
       <p className="mr-2 md:mr-8">
         {format_event_date(event.start?.dateTime, event.end?.dateTime, showEndTime)} &nbsp; {event.summary}
       </p>
-      {showMapLink && event.location && (
-        <a
-          href={`http://maps.google.com/?q=${event.location}`}
-          className="flex text-[min(1.5vw,10px)] items-center shadow-nps px-1.5 rounded-lg"
-        >
-          <p>＠{formatLocation(event.location)} &nbsp;</p>
-          <Image src="/googleMapsOfficialIcon.png" alt="Link Icon to Google Maps" width={8} height={8} />
-        </a>
-      )}
+      {showMapLink && event.location && <MapLink placeName={event.location} />}
     </div>
   );
 }
