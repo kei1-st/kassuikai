6e04151afe17d764bafc61060307356e75a08707
kei1-st <kei1satoh0603@gmail.com>
2024-04-04 18:45:53 +0900

    新歓イベントに開催場所を追加表示

--- components/TabContents/Tabs/EventTab/EventInfo.tsx
+++ components/TabContents/Tabs/EventTab/EventInfo.tsx
@@ -1,11 +1,32 @@
 import { calendar_v3 } from "googleapis";
+import Image from "next/image";
 
 import { format_event_date } from "@/scripts/format_date";
+import formatLocation from "@/scripts/format_location";
 
-export default function EventInfo({ event }: { event: calendar_v3.Schema$Event }) {
+export default function EventInfo({
+  event,
+  showEndTime,
+  showMapLink,
+}: {
+  event: calendar_v3.Schema$Event;
+  showEndTime: boolean;
+  showMapLink: boolean;
+}) {
   return (
-    <div className={`mx-5 my-2`}>
-      {format_event_date(event.start?.dateTime, event.end?.dateTime)} &nbsp; {event.summary}
+    <div className={`ml-5 my-2 flex`}>
+      <p className="mr-2">
+        {format_event_date(event.start?.dateTime, event.end?.dateTime, showEndTime)} &nbsp; {event.summary}
+      </p>
+      {showMapLink && event.location && (
+        <a
+          href={`http://maps.google.com/?q=${event.location}`}
+          className="flex text-[min(1.5vw,10px)] items-center shadow-nps px-1.5 rounded-lg"
+        >
+          <p>＠{formatLocation(event.location)} &nbsp;</p>
+          <Image src="/googleMapsOfficialIcon.png" alt="Link Icon to Google Maps" width={8} height={8} />
+        </a>
+      )}
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,7 @@ const config: Config = {
       boxShadow: {
         p: "1px 1px 3px 0px rgba(171, 171, 171, 0.90) inset, -1px -1px 2px 0px rgba(255, 255, 255, 0.90) inset, 1px -1px 2px 0px rgba(171, 171, 171, 0.20) inset, -1px 1px 2px 0px rgba(171, 171, 171, 0.20) inset, -1px -1px 2px 0px rgba(171, 171, 171, 0.50), 1px 1px 2px 0px rgba(255, 255, 255, 0.30)",
         np: "5px 5px 13px 0px rgba(220, 220, 220, 0.90), -5px -5px 10px 0px rgba(255, 255, 255, 0.90), 5px -5px 10px 0px rgba(220, 220, 220, 0.20), -5px 5px 10px 0px rgba(220, 220, 220, 0.20), -1px -1px 2px 0px rgba(220, 220, 220, 0.50) inset, 1px 1px 2px 0px rgba(255, 255, 255, 0.30) inset",
+        nps: "5px 5px 13px 0px rgba(220, 220, 220, 0.90), -5px -5px 10px 0px rgba(255, 255, 255, 0.90), 5px -5px 10px 0px rgba(220, 220, 220, 0.20), -5px 5px 8px 0px rgba(220, 220, 220, 0.60), -1px -1px 2px 0px rgba(220, 220, 220, 0.70) inset, 1px 1px 2px 0px rgba(255, 255, 255, 0.30) inset",
         contents:
           "11px 11px 28px 0px rgba(205, 205, 205, 0.90) inset, -11px -11px 22px 0px rgba(255, 255, 255, 0.90) inset, 11px -11px 22px 0px rgba(205, 205, 205, 0.20) inset, -11px 11px 22px 0px rgba(205, 205, 205, 0.20) inset, -1px -1px 2px 0px rgba(205, 205, 205, 0.50), 1px 1px 2px 0px rgba(255, 255, 255, 0.30)",
       },
